c36d99cd5a20aa7d9cc4dc1ddf3a5dfaab009ba4
d601 <shmeep@gmail.com>
2014-11-15 20:29:39 -0800

    Draw a rectangle

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
 #include <SFML/Graphics.hpp>
 
 /* Copied right out of the SFML tutorial. */
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Karellan");
+
+    sf::Texture texture;
+    if (!texture.create(64, 32)) {
+        return -1;
+    }
+
+    // I couldn't figure out how to get an sf::Color into an array of ints.
+    sf::Uint8* pixels = new sf::Uint8[64 * 32 * 4];
+    for (int i = 0; i < 64 * 32 * 4; i += 4) {
+        pixels[i] = 0xff;
+        pixels[i + 1] = 0xff;
+        pixels[i + 2] = 0x0;
+        pixels[i + 3] = 0xff;
+    }
+    texture.update(pixels);
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    sprite.setPosition(sf::Vector2f(32, 32));
 
     while (window.isOpen())
     {
@@ -17,10 +37,12 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::Black);
+        window.draw(sprite);
         window.display();
     }
+
+    delete pixels;
 
     return 0;
 }
